f9b81f3d50f1ce5ee48650ad65c48ff9a56bc832
Tony Kibling <126832027+tptony@users.noreply.github.com>
2024-11-13 09:24:17 -0500

    Update streamlit_app.py
    actual thumbs up icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon=":thumbs_up")
+        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="👍")
