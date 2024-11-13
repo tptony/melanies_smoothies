0559aaf9d23a4c29304b658c46b3d61b17053ea2
Tony Kibling <126832027+tptony@users.noreply.github.com>
2024-11-13 09:36:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="👍")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response).json()
+st.text(smoothiefroot_response.json())
